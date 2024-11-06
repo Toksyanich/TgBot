ab9528f1a136a8443b1db68883707a586d477051
Den <dend51108@gmail.com>
2024-11-06 20:32:15 +0300

    added  formula

--- calculator.py
+++ calculator.py
@@ -1,1 +1,7 @@
-a = 'hello world'
+weight = int(input("Input weight: "))
+height = int(input("Input height: "))
+age = int(input("Input age: "))
+
+DCI = (weight + height - age) * 1.3
+
+print((int (DCI)))
